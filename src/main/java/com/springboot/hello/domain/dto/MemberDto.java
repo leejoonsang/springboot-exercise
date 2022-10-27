e0d3f5ee89e264177c34b885a6ea4f484cd8dc4a
Sangjoon <djm04212@naver.com>
2022-10-27 13:16:23 +0900

    DTO 객체를 활용한 GET 메서드 구현

---
+++ src/main/java/com/springboot/hello/domain/dto/MemberDto.java
@@ -0,0 +1,44 @@
+package com.springboot.hello.domain.dto;
+
+public class MemberDto {
+    private String name;
+    private String email;
+    private String organization;
+
+    public MemberDto(String name, String email, String organization){
+        this.name = name;
+        this.email = email;
+        this.organization = organization;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    @Override
+    public String toString(){
+        return "MemberDto(" + "name = " + name + "\n" +
+                "email = " + email + "\n" +
+                "organization = " + organization + "\n" + ")";
+    }
+}
