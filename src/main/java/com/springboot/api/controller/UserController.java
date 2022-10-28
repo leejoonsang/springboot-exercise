d6ba3b30a82d212230d5fe21d65ef6fa506838de
Sangjoon <djm04212@naver.com>
2022-10-28 15:26:48 +0900

    [추가] UserController

---
+++ src/main/java/com/springboot/api/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dao.UserDao;
+import com.springboot.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class UserController {
+
+    private final UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    @PostMapping("/user")
+    public String add(@RequestBody User user) {
+        userDao.add(user);
+        return String.format("%s번 등록", user.getId());
+    }
+
+    @DeleteMapping(value = "/user/all")
+    public String deleteAll() {
+        userDao.deleteAll();
+        return "전체 삭제 완료.";
+    }
+}
