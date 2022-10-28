f2a3ee428a9151509a8ab8f8de4c2072f0aded13
Sangjoon <djm04212@naver.com>
2022-10-28 14:27:43 +0900

    [수정] dataSource 삭제

--- src/main/java/com/springboot/api/dao/UserDao.java
+++ src/main/java/com/springboot/api/dao/UserDao.java
@@ -1,29 +1,21 @@
 package com.springboot.api.dao;
 
-import com.springboot.api.domain.user.User;
+import com.springboot.api.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Map;
 
 @Component
 public class UserDao {
-    private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public void add(User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?);",
                 user.getId(), user.getName(), user.getPassword());
     }
 
