193b48e8e36a5fc00803edf1cc8201199a9259ad
Sangjoon <djm04212@naver.com>
2022-10-31 15:43:32 +0900

    [수정] 전국 병의원 정보 데이터 처리 (진행) - 에러 해결 중..

--- src/test/java/com/springboot/api/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/api/parser/HospitalParserTest.java
@@ -3,17 +3,37 @@ package com.springboot.api.parser;
 import com.springboot.api.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class HospitalParserTest {
 
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
 
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
     @Test
-    @DisplayName("csv 1줄을 Hospital 로 잘 만드는지 test")
+    @DisplayName("데이터가 10만건 이상(초과) 파싱 됐는지 Test")
+    void name() throws IOException {
+        String filename = "/Users/sangjoonlee/Desktop/BES2/fulldata_01_01_02_P_의원.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 100000);
+        for(int i=0; i<10; i++){
+            System.out.println(hospitalList.get(i).getHospitalName());
+        }
+        System.out.printf("파싱된 데이터 개수:%d", hospitalList.size());
+    }
+
+    @Test
+    @DisplayName("csv 1줄을 Hospital 로 잘 만드는지 Test")
     void convertToHospital() {
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
