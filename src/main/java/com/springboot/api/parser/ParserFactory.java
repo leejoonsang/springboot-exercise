4dd18290e1461e37afa39ba996d5938960f465cd
Sangjoon <djm04212@naver.com>
2022-11-02 02:27:33 +0900

    Factory

--- src/main/java/com/springboot/api/parser/ParserFactory.java
+++ src/main/java/com/springboot/api/parser/ParserFactory.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ParserFactory {
     @Bean
-    public ReadLineContext<Hospital> readLineContext() {
+    public ReadLineContext<Hospital> hospitalReadLineContext() {
         return new ReadLineContext<Hospital>(new HospitalParser());
     }
 }
